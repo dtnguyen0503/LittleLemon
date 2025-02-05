c873b068d6161bb4be94e18dc67e8403b6b12783
dtnguyen0503 <nguyendat9453@gmail.com>
2025-02-05 13:39:11 -0600

    adding serializers.py

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Menu, Booking 
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['url', 'username', 'email', 'group']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking 
+        fields = '__all__'
